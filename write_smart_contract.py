eeef85c047ec0a870ff2705a70e3719ec53013cf
Far-Aghili <57596443+Far-Aghili@users.noreply.github.com>
2020-12-08 15:15:00 +0100

    Update write_smart_contract.py

--- write_smart_contract.py
+++ write_smart_contract.py
@@ -7,19 +7,19 @@ HashOLABorCLAB=hashlib.sha256(b"PID_T\|PID_R\|X").hexdigest()
 # Set up web3 connection with Ganache
 ganache_url = "http://127.0.0.1:7545"
 web3 = Web3(Web3.HTTPProvider(ganache_url))
+ID=0# Based on Identity of the tag
+# TODO: Deploy the Greeter contract to Ganache with remix.ethereum.org
 
-# Set a default account to sign transactions
-web3.eth.defaultAccount = web3.eth.accounts[0]
-# Greeter contract ABI
-abi = json.loads('[{"constant":false,"inputs":[{"name":"_greeting","type":"string"}],"name":"setGreeting","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"greet","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"greeting","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}]')
+# Set a default account to sign transactions - this account is unlocked with Ganache
+web3.eth.defaultAccount = web3.eth.accounts[ID]
+# Greeter contract ABI # based on your smart contract
+abi = json.loads('[{"inputs": [],"name": "Greeter","outputs": [],"stateMutability": "nonpayable","type": "function"},{"inputs": [],"name": "greet","outputs": [{"internalType": "string","name": "","type": "string"}],"stateMutability": "view","type": "function"},{"inputs": [],"name": "greeting","outputs": [{"internalType": "string","name": "","type": "string"}],"stateMutability": "view","type": "function"},{"inputs": [{"internalType": "string","name": "_greeting","type": "string"}],"name": "setGreeting","outputs": [],"stateMutability": "nonpayable","type": "function"}]')
 # Greeter contract address - convert to checksum address
-address = web3.toChecksumAddress('https://mainnet.infura.io/v3')
+address = web3.toChecksumAddress('0x492EC9c3cB5358A9969f520b2D1b97f0824AC82B') # based on your address
 # Initialize contract
 contract = web3.eth.contract(address=address, abi=abi)
-# Read the default greeting
-print(contract.functions.greet().call())
 # Set a new greeting
-tx_hash = contract.functions.setGreeting(HashOLABorCLAB).transact()
+tx_hash = contract.functions.setGreeting('New Id2').transact()
 # Wait for transaction to be mined
 web3.eth.waitForTransactionReceipt(tx_hash)
 # Display the new greeting value
